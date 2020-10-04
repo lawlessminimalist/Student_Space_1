840ce4d638e4e06483f4bd69f4d52b49e241b204
AidenKinging <aidenkinging@gmail.com>
2020-10-04 15:46:41 +1000

    Added CalendarMonth View and updated AppShell to route to the correct Views.

--- Student_Space_1/Student_Space_1/AppShell.xaml.cs
+++ Student_Space_1/Student_Space_1/AppShell.xaml.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using Student_Space.ViewModels;
 using Student_Space.Views;
 using Xamarin.Forms;
+using Student_Space_1.Views;
 
 namespace Student_Space
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
+
+        Dictionary<string, Type> routes = new Dictionary<string, Type>();
+        public Dictionary<string, Type> Routes {  get { return routes;  } }
+
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutes();
+            BindingContext = this;
 
+        }
+
+        void RegisterRoutes()
+        {
+            routes.Add("CalendarMonth", typeof(CalendarMonth));
+
+            foreach (var item in routes)
+            {
+                Routing.RegisterRoute(item.Key, item.Value);
+            }
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
