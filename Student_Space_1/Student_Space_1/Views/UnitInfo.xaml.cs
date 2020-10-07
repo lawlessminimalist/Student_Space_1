0ee15d3f76a8bc5008ae5fef750859c03d38e31d
JoelWestNewman <55678052+JoelWestNewman@users.noreply.github.com>
2020-10-07 12:15:01 +1000

    unit info view model added, major changes to unit info page, pickers and popups implemented

--- Student_Space_1/Student_Space_1/Views/UnitInfo.xaml.cs
+++ Student_Space_1/Student_Space_1/Views/UnitInfo.xaml.cs
@@ -16,5 +16,31 @@ namespace Student_Space_1.Views
         {
             InitializeComponent();
         }
+        async void OnDisplayAlertButtonClicked2031(object sender, EventArgs e)
+        {
+            await DisplayAlert("CAB 203 Announcement number 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Close");
+        }
+
+        async void OnDisplayAlertButtonClicked2032(object sender, EventArgs e)
+        {
+            await DisplayAlert("CAB 203 Announcement number 2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Close");
+        }
+
+        async void OnDisplayAlertButtonClicked2033(object sender, EventArgs e)
+        {
+            await DisplayAlert("CAB 203 Announcement number 3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Close");
+        }
+
+        async void OnDisplayAlertButtonClicked2034(object sender, EventArgs e)
+        {
+            await DisplayAlert("CAB 203 Announcement number 4", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Close");
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+
     }
 }
