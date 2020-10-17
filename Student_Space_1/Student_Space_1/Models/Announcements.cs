2d9282bdf0207a18f888f048fe6d51b67d288bf8
JoelWestNewman <55678052+JoelWestNewman@users.noreply.github.com>
2020-10-18 03:21:47 +1000

    unit information binding alert and picker, sizing menu changes, adding announcements model

---
+++ Student_Space_1/Student_Space_1/Models/Announcements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Space_1.Models
+{
+    public class Announcement
+    {
+		public string AnnouncementName { get; set; }
+
+		public string AnnouncementInfo { get; set; }
+
+		public override string ToString()
+		{
+			return AnnouncementName;
+		}
+	}
+}
--- Student_Space_1/Student_Space_1/ViewModels/UnitInfoViewModel.cs
+++ Student_Space_1/Student_Space_1/ViewModels/UnitInfoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Student_Space_1.Models;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,48 +7,94 @@ using System.ComponentModel;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Student_Space_1.Models;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace Student_Space.ViewModels
 {
     public class UnitInfoViewModel : BaseViewModel
     {
-        
-        
-        
-        
-        List<Monkey> Monkeys;
-        public class Monkey
-        {
 
-            public string Name { get; set; }
-            public string Location { get; set; }
-            public string Details { get; set; }
+
+        public ICommand PickerAlert4 { get; set; }
+        public ObservableCollection<Announcement> Announcements{ get; set; }
+
+        Announcement selectedAnnouncement;
+        public Announcement SelectedAnnouncement
+        {
+            get { return selectedAnnouncement; }
+            set
+            {
+                if (selectedAnnouncement != value)
+                {
+                    selectedAnnouncement = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-    
-
-    public UnitInfoViewModel()
+        void MakeAlter()
         {
+            Application.Current.MainPage.DisplayAlert("Alert", SelectedAnnouncement.AnnouncementInfo, "Cancel", "ok");
+        }
 
-            Monkeys = new List<Monkey>() {
-        new Monkey() {Name = "monkey1", Details = "details", Location = "brisbane" },
-        new Monkey() {Name = "monkey2", Details = "details", Location = "brisbane" },
-        new Monkey() {Name = "monkey3", Details = "details", Location = "brisbane" }
 
-    };
 
+        public UnitInfoViewModel()
+        {
 
             Title = "Unit Information";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
-
+            PickerAlert4 = new Command(get => MakeAlter());
+            SetupData();
 
 
     }
 
 
+
+
         public ICommand OpenWebCommand { get; }
 
-        
+
+
+
+        void SetupData()
+        {
+            Announcements = new ObservableCollection<Announcement>()
+            {
+                new Announcement
+            {
+                AnnouncementName = "Announcement numero uno",
+                AnnouncementInfo = "This is the first announcement info page i really don tknow what else to write on this",
+
+            },
+                new Announcement
+            {
+                AnnouncementName = "Announcement numero twqo",
+                AnnouncementInfo = "This is the first announcement info page i really don tknow what else to write on this",
+
+            },
+                new Announcement
+            {
+                AnnouncementName = "Announcement numero three",
+                AnnouncementInfo = "This is the first announcement info page i really don tknow what else to write on this",
+
+            },
+                new Announcement
+            {
+                AnnouncementName = "Announcement numero four",
+                AnnouncementInfo = "This is the first announcement info page i really don tknow what else to write on this",
+                
+            }
+
+            };
+        }
+
+
+
 
     }
 }
